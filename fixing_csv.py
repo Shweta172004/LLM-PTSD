4e62134ad38fd12c04c5d7cf44e8c648acafd4d7
Shweta172004 <122981592+Shweta172004@users.noreply.github.com>
2024-08-28 22:30:22 +0530

    Update fixing_csv.py
    This code renames the csv for convenience and drops columns that were not necessary.

--- fixing_csv.py
+++ fixing_csv.py
@@ -1,23 +1,17 @@
 import pandas as pd
 import numpy as np
 import math
-
-df1 = pd.read_csv("C:\\Users\\shweta patel\\OneDrive\\Desktop\\FINAL\\patient_info.csv")
+df1 = pd.read_csv("path_to_patients_info.csv")
 df1['age'] = df1['age'].apply(round)
-df1.to_csv("C:\\Users\\shweta patel\\OneDrive\\Desktop\\FINAL\\demographics.csv", index=False)
+df1.to_csv("path_of_demographics.csv", index=False)
 
-# print(df1.head())
+df2 = pd.read_csv("path_to_omr.csv")
+df2.to_csv("path_to_medical_histories.csv" , index = False)
 
-df2 = pd.read_csv("C:\\Users\\shweta patel\\OneDrive\\Desktop\\FINAL\\omr.csv")
-df2.to_csv("C:\\Users\\shweta patel\\OneDrive\\Desktop\\FINAL\\medical_histories.csv" , index = False)
-# print(df2.head())
-
-df3 = pd.read_csv("C:\\Users\\shweta patel\\OneDrive\\Desktop\\FINAL\\lab_events.csv")
+df3 = pd.read_csv("path_to_lab_events.csv")
 df3.drop(['value' , 'hadm_id', 'order_provider_id'], axis =1)
-df3.to_csv("C:\\Users\\shweta patel\\OneDrive\\Desktop\\FINAL\\lab_events.csv" , index = False)
-# print(df3.head())
 
-df4 = pd.read_csv("C:\\Users\\shweta patel\\OneDrive\\Desktop\\FINAL\\emar.csv")
+df4 = pd.read_csv("path_to_emar.csv")
 df4.drop(['poe_id', 'pharmacy_id','enter_provider_id'] , axis = 1)
-df4.to_csv("C:\\Users\\shweta patel\\OneDrive\\Desktop\\FINAL\\medical_regimens.csv" , index = False)
+df4.to_csv("path_to_medical_regimens.csv" , index = False)
 
