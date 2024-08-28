9f3942f0be74090e59ddb00f40839185b5af0d77
Shweta172004 <122981592+Shweta172004@users.noreply.github.com>
2024-08-28 22:46:38 +0530

    Update final_model.py

--- final_model.py
+++ final_model.py
@@ -89,10 +89,3 @@ if uploaded_files:
             for i in range(len(st.session_state['generated'])):
                 message(st.session_state['past'][i] , is_user = True, key = str[i]+'_user')
                 message(st.session_state['generated'][i], key = str[i])                   
-
-        
-
-
-
-    
-
